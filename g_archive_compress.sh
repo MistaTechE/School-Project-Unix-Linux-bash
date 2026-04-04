9a01e60fe7f5364f85f38356ed2b551be9165f54
Evan Meserve <emeser3@wgu.edu>
2026-04-04 07:10:11 +0000

    Edit g_archive_compress.sh added code

--- g_archive_compress.sh
+++ g_archive_compress.sh
@@ -4,3 +4,34 @@
 #D796 - Unix and Linux
 # Author: Evan Meserve
 # Student ID 011285590
+
+#1. function to get file size
+fileSize() {
+    FILE="$1"
+    if [ -f "$FILE" ]; then
+        stat -c%s "$FILE"
+    else
+        echo 0
+    fi
+}
+
+#2. archive and compress with gzip
+GZIP_FILE="etc_archive.tar.gz"
+sudo tar -czf "$GZIP_FILE" /etc
+
+#3. archive and compress with bzip2
+BZIP_FILE="etc_archive.tar.bz2"
+sudo tar -cjf "$BZIP_FILE" /etc
+
+#4. calculate sizes
+GZIP_SIZE=$(fileSize "$GZIP_FILE")
+BZIP_SIZE=$(fileSize "$BZIP_FILE")
+
+#5. print the difference in size between compression algorithms
+DIFF=$((GZIP_SIZE - BZIP_SIZE))
+echo "Size difference (gzip - bzip2): $DIFF bytes"
+els
+    echo "Both archives are the same size."
+fi
+
+echo "Archive and compression compl
