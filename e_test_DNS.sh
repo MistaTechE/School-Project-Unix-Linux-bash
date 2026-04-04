4773ddb5bdbee9654e04716b3b651a40b7189217
Evan Meserve <emeser3@wgu.edu>
2026-04-04 03:28:24 +0000

    Edit e_test_DNS.sh adding code

--- e_test_DNS.sh
+++ e_test_DNS.sh
@@ -4,3 +4,12 @@
 #D796 - Unix and Linux
 # Author: Evan Meserve
 # Student ID 011285590
+
+TARGET="8.8.8.8"
+
+# Ping once 2 sec timeout
+if ping -c 1 -W 2 "$TARGET" >/dev/null 2>&1; then
+    echo "Connection to $TARGET is successful."
+else
+    echo "Cannot reach $TARGET."
+fi
