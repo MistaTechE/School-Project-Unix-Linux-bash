50c088caa6df1ec8520ae6e78e8ec03fb5d35154
Evan Meserve <emeser3@wgu.edu>
2026-04-04 03:32:25 +0000

    Edit e_test_connection.sh added code

--- e_test_connection.sh
+++ e_test_connection.sh
@@ -4,3 +4,13 @@
 #D796 - Unix and Linux
 # Author: Evan Meserve
 # Student ID 011285590
+
+DOMAIN="example.com"
+
+# nslookup resolve domain
+if nslookup "$DOMAIN" >/dev/null 2>&1; then
+    IP=$(nslookup "$DOMAIN" | awk '/^Address: / { print $2 }')
+    echo "DNS resolution successful. $DOMAIN resolves to $IP"
+else
+    echo "DNS resolution failed for $DOMAIN."
+fi
