4046777d88c052a1d52c02fa935845eeed390df8
MistaTechE <74339419+MistaTechE@users.noreply.github.com>
2026-04-30 13:20:46 -1000

    Update d_1_install_vim.sh

--- d_1_install_vim.sh
+++ d_1_install_vim.sh
@@ -1,9 +1,6 @@
 #d_1_install_vim.sh
 #!/bin/bash
-#WGU
-#D796 - Unix and Linux
-# Author: Evan Meserve
-# Student ID 011285590
+
 
 # detect package manager and install Vim
 if command -v vim >/dev/null 2>&1; then
