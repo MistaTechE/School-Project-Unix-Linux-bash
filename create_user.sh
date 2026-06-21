3dfcc7086c0f4bc3098581ec22b0a3ab95b4dcac
MistaTechE <74339419+MistaTechE@users.noreply.github.com>
2026-06-20 20:04:13 -1000

    Update create_user.sh

--- create_user.sh
+++ create_user.sh
@@ -1,2 +1,26 @@
-#create_user.sh
 #!/bin/bash
+#create_user.sh
+
+#1.  The script must take an argument for the username and verify that the argument is provided by producing an error message if it is not.
+
+if [[ $EUID -ne 0 ]]; then
+    exec sudo "$0" "$@"
+fi
+
+USERNAME="${1-}"
+
+if [[ -z "$USERNAME" ]]
+then
+    echo "Error: No username argument provided."
+    exit 1
+fi
+
+#2.  If the group "dev_group" does not exist, create it.
+
+groupadd -f dev_group
+
+#3.  The script must add a user and assign a password.
+
+useradd -m -G dev_group "$USERNAME"
+chpasswd <<< "${USERNAME}:tempPassword"
+passwd -e "$USERNAME"
